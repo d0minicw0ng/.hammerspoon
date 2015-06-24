b9435942a98f6ab56b9ffa6502bf7ba08c9dac02
Dominic Wong <dominic.wong.617@gmail.com>
2015-06-24 08:13:54 +0800

    Resize window to full screen hotkey + auto reload

--- init.lua
+++ init.lua
@@ -117,3 +117,31 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
   f.h = max.h / 2
   win:setFrame(f)
 end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Space", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+function reloadConfig(files)
+  doReload = false
+  for _, file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Hello! Mr. Dominic Wong")
